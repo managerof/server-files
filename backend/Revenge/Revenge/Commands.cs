0853f8a2a44c2e1fd060e9d71a354827439424b9
managerof <109286391+managerof@users.noreply.github.com>
2022-11-26 12:16:54 +0000

    ok

---
+++ backend/Revenge/Revenge/Commands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace Revenge
+{
+    class Commands : Script
+    {
+        [Command("veh", Description = "/vehicle id color1 color2", Alias = "vehicle")]
+        private void cmd_veh(Player player, string vehname, int color1, int color2)
+        {
+            uint vhash = NAPI.Util.GetHashKey(vehname);
+            if (vhash <= 0)
+            {
+                player.SendChatMessage("~r~Unknown vehicle model");
+                return;
+            }
+            Vehicle veh = NAPI.Vehicle.CreateVehicle(vhash, player.Position, player.Heading, color1, color2);
+            veh.NumberPlate = "16REVENGERP";
+            veh.Locked = false;
+            veh.EngineStatus = true;
+            player.SetIntoVehicle(veh, (int)VehicleSeat.Driver);
+        }
+    }
+}
---
+++ backend/Revenge/Revenge/Events.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace Revenge
+{
+    class Events : Script
+    {
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SendChatMessage("Welocome to Revenge ~r~Role Play");
+        }
+
+        [ServerEvent(Event.PlayerSpawn)]
+        private void OnPlayerSpawn(Player player)
+        {
+            player.Health = 50;
+            player.Armor = 50;
+        }
+    }
+}
